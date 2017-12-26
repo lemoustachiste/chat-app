8c650f15b296e87d6e2fe68312aea5aa12bad311
Julien Fraichot <julien@typeform.com>
2017-12-26 12:38:18 +0100

    feat(Message): add basic component

--- src/components/atoms/Message/Message.js
+++ src/components/atoms/Message/Message.js
@@ -1,4 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const Message = ({ text }) => {
+  return (
+    <p className='chat-c-message'>
+      { text }
+    </p>
+  )
+}
 
+Message.propTypes = {
+  text: PropTypes.string
+}
+
+export default Message
--- src/components/atoms/Message/Message.test.js
+++ src/components/atoms/Message/Message.test.js
@@ -1,1 +1,12 @@
-describe('', function () {})
+import React from 'react'
+import { shallow } from 'enzyme'
+import Message from './Message'
+
+describe('<Message/> test suite', function () {
+  describe('given it receives a text as a prop', function () {
+    it('renders a text', function () {
+      const wrapper = shallow(<Message text='this is a test' />)
+      expect(wrapper.text()).toContain('this is a test')
+    })
+  })
+})
