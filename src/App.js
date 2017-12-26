8205d5af93b87d830334c91948756e595953bac4
Julien Fraichot <julien@typeform.com>
2017-12-26 16:34:49 +0100

    feat(Styles): start working with CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { configureStore, getInitialState } from './store'
 import { ChatBoard } from './components/organisms'
+require('./css/bundle.css')
 
 class App extends Component {
   constructor () {
