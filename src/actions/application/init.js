43fd02b60f9198e9bc5f3749c22ffdca3e1f0288
Julien Fraichot <julien@typeform.com>
2017-12-27 12:55:46 +0100

    feat(Storage): read from storage on app init

--- src/actions/application/init.js
+++ src/actions/application/init.js
@@ -3,12 +3,31 @@ import * as actions from '../'
 import { RECEIVING_EXTERNAL_MESSAGE } from '../../constants/chatServerEvents'
 import domain from '../../domain'
 
+const storedProperties = [
+  'messages',
+  'buddyNickname'
+]
+
+function readFromStorage () {
+  return storedProperties.reduce((acc, key) => {
+    const value = domain.storage.read(key)
+
+    if (value != null) {
+      acc[key] = value
+    }
+
+    return acc
+  }, {})
+}
+
 export default function init () {
   return (dispatch) => {
     domain.chat.connect()
     domain.chat.subscribe(RECEIVING_EXTERNAL_MESSAGE, (data) => {
       dispatch(actions.messages.registerExternalMessage(data))
     })
+
+    const retrievedDataFromStorage = readFromStorage()
 
     dispatch({
       type: ACTIONS.INIT
